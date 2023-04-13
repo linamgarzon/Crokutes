b0cdc7402b94a932a27b0a3edbed8f967c0c296a
Lina Garzon <garzonlina35@gmail.com>
2023-04-13 12:55:03 -0500

    Separacion de codigo, agradable visualmente

--- Javascript/service/product-server.js
+++ Javascript/service/product-server.js
@@ -1,28 +1,7 @@
-const crearNuevoProducto = (url, nombre, precio) => {
-  const linea = document.createElement("div");
-  const contenido = `
-  <div class="producto">
-    <img class="producto_imagen" src="${url}" alt="Purina">
-    <span class="producto_descripcion">${nombre}</span>
-    <span class="producto_precio" data-precio>${precio}</span>
-    <a class="producto_boton" href="#">Ver producto</a>
- </div>`;
-  linea.innerHTML = contenido;
-  return linea;
-};
-const seccion = document.querySelector("[data-botones-Cantidad]");
-
 //funcion nativa fetch que genera una promesa
-const listaProducto = () => fetch('http://localhost:3000/producto').then((respuesta)=> 
-    respuesta.json()
-    );
-listaProducto().then((data) => {
-  data.forEach((producto) => {
-    const nuevaLinea = crearNuevoProducto(
-      producto.url,
-      producto.nombre,
-      producto.precio
-    );
-    seccion.appendChild(nuevaLinea);
-  });
-}).catch((error)=> alert("Ocurrio un error" + error));
+const listaProducto = () =>
+  fetch("http://localhost:3000/producto").then((respuesta) => respuesta.json());
+
+export const productService ={
+    listaProducto,
+}
---
+++ controllers/product.controller.js
@@ -0,0 +1,30 @@
+import { productService } from "../Javascript/service/product-server.js";
+
+console.log(productService);
+
+const crearNuevoProducto = (url, nombre, precio) => {
+    const linea = document.createElement("div");
+    const contenido = `
+    <div class="producto">
+      <img class="producto_imagen" src="${url}" alt="Purina">
+      <span class="producto_descripcion">${nombre}</span>
+      <span class="producto_precio" data-precio>${precio}</span>
+      <a class="producto_boton" href="#">Ver producto</a>
+   </div>`;
+    linea.innerHTML = contenido;
+    return linea;
+  };
+  const seccion = document.querySelector("[data-botones-Cantidad]");
+
+  productService.listaProducto()
+  .then((data) => {
+    data.forEach((producto) => {
+      const nuevaLinea = crearNuevoProducto(
+        producto.url,
+        producto.nombre,
+        producto.precio
+      );
+      seccion.appendChild(nuevaLinea);
+    });
+  })
+  .catch((error) => alert("Ocurrio un error" + error));
