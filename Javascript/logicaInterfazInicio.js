d7282825e6ff8393fbd2cbd2338344f3e7507db6
Lina Garzon <garzonlina35@gmail.com>
2023-04-13 11:56:10 -0500

    servidor json

--- Javascript/logicaInterfazInicio.js
+++ Javascript/logicaInterfazInicio.js
@@ -5,15 +5,13 @@ function inicioSesion(){
 
     const email = document.querySelector('[data-email]').value;
     const password = document.querySelector('[data-password]').value;
-    console.log(email, password);
 
     bAcceso = validarCredenciales(email, password);
-    console.log(bAcceso);
     
     if (bAcceso == true) {
         ingresar();
     }
 }
 function ingresar(){
-    window.location.href = "/screens/index.html";
+    window.open('./index.html');
 }
---
+++ Javascript/pagina-principal.js
@@ -0,0 +1,6 @@
+const buttonlogin = document.querySelector('[data-buttonLogin]').addEventListener('click', ingresoAdministrador);
+
+function ingresoAdministrador(){
+    location.replace('/login.html');
+}
+
